f359e293580c92b60526a77dab59f70338e46087
Dave Hughes <dave@waveform.org.uk>
2012-10-17 16:18:52 +0100

    Updated setup.py with license and utilities
    Added our standard utils module to setup to have it derive version from
    the main package, added all the relevant classifiers and updated
    ownership and contact information (along with the usual work-around for
    Windows packaging)

--- setup.py
+++ setup.py
@@ -1,14 +1,64 @@
+# -*- coding: utf-8 -*-
+# vim: set et sw=4 sts=4:
+
+# Copyright 2012 Dave Hughes.
+#
+# This file is part of samplesdb.
+#
+# samplesdb is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# samplesdb is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# samplesdb.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import (
+    unicode_literals,
+    print_function,
+    absolute_import,
+    division,
+    )
+
 import os
-
 from setuptools import setup, find_packages
+from utils import description, get_version, require_python
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+# Workaround <http://bugs.python.org/issue10945>
+import codecs
+try:
+    codecs.lookup('mbcs')
+except LookupError:
+    ascii = codecs.lookup('ascii')
+    func = lambda name, enc=ascii: {True: enc}.get(name=='mbcs')
+    codecs.register(func)
 
-requires = [
-    'pyramid',
-    'SQLAlchemy',
+require_python(0x020600f0)
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(HERE, 'README.txt')).read()
+CHANGES = open(os.path.join(HERE, 'CHANGES.txt')).read()
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Web Environment',
+    'Framework :: Pyramid',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    'Topic :: Scientific/Engineering',
+    ]
+
+REQUIRES = [
+    'pyramid<1.4dev',
+    'SQLAlchemy<0.8dev',
     'transaction',
     'pyramid_tm',
     'pyramid_debugtoolbar',
@@ -16,30 +66,34 @@ requires = [
     'waitress',
     ]
 
-setup(name='samplesdb',
-      version='0.0',
-      description='samplesdb',
-      long_description=README + '\n\n' +  CHANGES,
-      classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Pylons",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
-      author='',
-      author_email='',
-      url='',
-      keywords='web wsgi bfg pylons pyramid',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      test_suite='samplesdb',
-      install_requires=requires,
-      entry_points="""\
-      [paste.app_factory]
-      main = samplesdb:main
-      [console_scripts]
-      initialize_samplesdb_db = samplesdb.scripts.initializedb:main
-      """,
-      )
+ENTRY_POINTS = """\
+    [paste.app_factory]
+    main = samplesdb:main
 
+    [console_scripts]
+    initialize_samplesdb_db = samplesdb.scripts.initializedb:main
+    """
+
+def main():
+    setup(
+        name                 = 'samplesdb',
+        version              = get_version(os.path.join(HERE, 'samplesdb/__init__.py')),
+        description          = 'samplesdb',
+        long_description     = description(os.path.join(HERE, 'README.txt')),
+        classifiers          = CLASSIFIERS,
+        author               = 'Dave Hughes',
+        author_email         = 'dave@waveform.org.uk',
+        url                  = 'https://github.com/waveform80/samplesdb',
+        keywords             = 'science samples database',
+        packages             = find_packages(exclude=['distribute_setup', 'utils']),
+        include_package_data = True,
+        platforms            = 'ALL',
+        install_requires     = REQUIRES,
+        extras_require       = {},
+        zip_safe             = False,
+        test_suite           = 'samplesdb',
+        entry_points         = ENTRY_POINTS,
+        )
+
+if __name__ == '__main__':
+    main()
