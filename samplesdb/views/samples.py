a1db881de24685f5883f915689453e43348e3e95
Dave Hughes <dave@waveform.org.uk>
2012-11-08 18:37:54 +0000

    Started work on samples views

---
+++ samplesdb/views/samples.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# vim: set et sw=4 sts=4:
+
+# Copyright 2012 Dave Hughes.
+#
+# This file is part of samplesdb.
+#
+# samplesdb is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# samplesdb is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# samplesdb.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import (
+    unicode_literals,
+    print_function,
+    absolute_import,
+    division,
+    )
+
+from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
+from formencode import validators, foreach
+
+from samplesdb.views import BaseView
+from samplesdb.forms import (
+    BaseSchema,
+    Form,
+    FormRenderer,
+    )
+from samplesdb.security import (
+    EDIT_COLLECTION,
+    )
+from samplesdb.models import (
+    DBSession,
+    Sample,
+    )
+
+
